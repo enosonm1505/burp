6dad35858f1e1320144d128e0f46f9d4566ed355
enosonm1505 <91190634+enosonm1505@users.noreply.github.com>
2022-07-06 21:00:22 +0530

    Update burp.py

--- burp.py
+++ burp.py
@@ -7,3 +7,5 @@ headers = {
 data = '{"application_logins":[{"password":"test","username":"test"}],"scan_configurations":[{"name":"Audit checks - light active","type":"NamedConfiguration"}],"urls":["https://example.com"]}'
 
 response = requests.post('http://10.15.60.71:8080/api/b2TGb96QOhqBmD6KdFRPtiwZUnzlU5LX/v0.1/scan', headers=headers, data=data)
+
+print(response.content)
